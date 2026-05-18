dcd13c4a1cfec42b1b877282c0c83d34e0f767fd
David <21690996+dporfirio@users.noreply.github.com>
2026-05-18 10:25:21 -0400

    updated news

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -193,6 +193,14 @@ declare module 'astro:content' {
   rendered?: RenderedContent;
   filePath?: string;
 }>;
+"projects": Record<string, {
+  id: string;
+  body?: string;
+  collection: "projects";
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
 "publications": Record<string, {
   id: string;
   render(): Render[".md"];
